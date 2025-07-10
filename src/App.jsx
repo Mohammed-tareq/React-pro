15a8f3466a7784ed43a9c7ccc8f5cd07fa3b3b94
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-07-10 20:31:10 +0300

    feat: add ProductCard component and clean up index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import ProductCard from './components/productCard/ProductCard'
 
 function App() {
 
   return (
     <>
-      
+      <ProductCard />
     </>
   )
 }  
---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,14 @@
+
+
+const ProductCard = ()=>{
+    return (
+        <div>
+            <h1>Product Card</h1>
+            <p>This is a product card component.</p>
+        </div>
+    )
+}
+
+
+
+export default ProductCard;
