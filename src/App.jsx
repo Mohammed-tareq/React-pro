625cd4b4b952bb9750d1a46a8f45949fd4a050c3
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-07-10 21:33:50 +0300

    feat: enhance ProductCard layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import ProductCard from './components/productCard/ProductCard'
 function App() {
 
   return (
-    <>
+    <div className='p-5 h-auto grid grid-cols-1'>
       <ProductCard />
-    </>
+    </div>
   )
 }  
 
